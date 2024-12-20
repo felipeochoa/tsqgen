91797e8c98c7f400f1440f63ab142c265505de21
Felipe Ochoa <felipe@incquery.com>
2024-12-19 21:29:38 -0500

    [dev] Don't run jest on build dir

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.ts'],
+    testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
     moduleFileExtensions: ['ts', 'js'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
